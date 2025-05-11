d730e498feb74cdc87cb6bfef46144c979f30d62
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-11 17:13:13 -0500

    Add transaction ports

--- demo/src/main/java/com/drtx/demo/core/ports/in/transaction/PaymentMethodServicePort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/in/transaction/PaymentMethodServicePort.java
@@ -1,4 +1,13 @@
 package com.drtx.demo.core.ports.in.transaction;
 
+import com.drtx.demo.core.model.transaction.PaymentMethod;
+import com.drtx.demo.core.model.transaction.PaymentMethodType;
+
+import java.util.List;
+
 public interface PaymentMethodServicePort {
+    PaymentMethod createPaymentMethod(PaymentMethod method);
+    PaymentMethod updatePaymentMethod(PaymentMethod method);
+    List<PaymentMethod> findAllByUser(Long userId);
+    void deletePaymentMethod(Long id);
 }
--- demo/src/main/java/com/drtx/demo/core/ports/in/transaction/TransactionServicePort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/in/transaction/TransactionServicePort.java
@@ -1,4 +1,15 @@
 package com.drtx.demo.core.ports.in.transaction;
 
+import com.drtx.demo.core.model.transaction.PaymentMethodType;
+import com.drtx.demo.core.model.transaction.Transaction;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TransactionServicePort {
+    Transaction createTransaction(Transaction transaction);
+    Transaction findTransactionById(Long id);
+    List<Transaction> findAllByUser(Long userId);
+    List<Transaction> findTransactionsBetweenDates(Long userId, LocalDateTime from, LocalDateTime to);
+    void cancelTransaction(Long transactionId);
 }
