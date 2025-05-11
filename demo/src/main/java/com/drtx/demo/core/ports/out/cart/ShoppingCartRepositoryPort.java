c4ee1606d2b8a8d105c253fe1036db92227e913f
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-11 17:58:38 -0500

    Add cart repository port

--- demo/src/main/java/com/drtx/demo/core/ports/out/cart/ShoppingCartRepositoryPort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/out/cart/ShoppingCartRepositoryPort.java
@@ -1,4 +1,21 @@
 package com.drtx.demo.core.ports.out.cart;
 
+import com.drtx.demo.core.model.cart.CartItem;
+import com.drtx.demo.core.model.cart.ShoppingCart;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ShoppingCartRepositoryPort {
+
+    ShoppingCart save(ShoppingCart cart);
+    Optional<ShoppingCart> findByUserId(Long userId);
+    Optional<ShoppingCart> findById(Long cartId);
+    void deleteById(Long cartId);
+
+    void addItem(Long cartId, CartItem item);
+    void removeItem(Long cartId, Long itemId);
+    boolean existsItemInCart(Long cartId, Long publicationId);
+    void clearCart(Long cartId);
+    List<CartItem> findItemsInCart(Long cartId);
 }
