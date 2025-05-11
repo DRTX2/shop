ca19789ce2633c12a076c59e959beeb0d0dab278
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-11 17:35:45 -0500

    Add cart ports

--- demo/src/main/java/com/drtx/demo/core/model/cart/CartItem.java
+++ demo/src/main/java/com/drtx/demo/core/model/cart/CartItem.java
@@ -2,10 +2,13 @@ package com.drtx.demo.core.model.cart;
 
 import com.drtx.demo.core.model.catalog.Publication;
 
+import java.math.BigDecimal;
+
 public class CartItem {
     private Long id;
     private Integer quantity;
 
     private ShoppingCart cart;
     private Publication publication;
+    private BigDecimal currentPrice;
 }
