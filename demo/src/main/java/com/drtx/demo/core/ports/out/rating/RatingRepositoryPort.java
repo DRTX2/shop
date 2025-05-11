a4825a89dd6a900e7b99afe020d8eac6eb309b48
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-11 17:59:27 -0500

    Add rating repository port

--- demo/src/main/java/com/drtx/demo/core/ports/out/rating/RatingRepositoryPort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/out/rating/RatingRepositoryPort.java
@@ -1,4 +1,15 @@
 package com.drtx.demo.core.ports.out.rating;
 
+import com.drtx.demo.core.model.rating.Rating;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface RatingRepositoryPort {
+    Rating save(Rating rating);
+    Optional<Rating> findById(Long id);
+    List<Rating> findByUserId(Long userId);
+    List<Rating> findByPublicationId(Long publicationId);
+    double calculateAverageRatingForUser(Long userId);
+    double calculateAverageRatingForPublication(Long publicationId);
 }
