7bac02f622b9650eaddea4c29e4d9e8a68b1d8d3
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-11 17:26:16 -0500

    Edit catalog ports

--- demo/src/main/java/com/drtx/demo/core/ports/in/catalog/CategoryServicePort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/in/catalog/CategoryServicePort.java
@@ -2,9 +2,16 @@ package com.drtx.demo.core.ports.in.catalog;
 
 import com.drtx.demo.core.model.catalog.Category;
 
+import java.util.List;
+
 public interface CategoryServicePort {
-   Category createCategory(Category category);
-   Category editCategory(Category category);
-   Category fetchAllCategories();// en el front creo que podria ordenarlas por las mas usadas
-   void deleteCategory(Long categoryId);
+    Category createCategory(Category category);
+
+    Category editCategory(Category category);
+
+    Category findCategoryById(Long id);
+
+    List<Category> fetchAllCategories();// en el front creo que podria ordenarlas por las mas usadas
+
+    void deleteCategory(Long categoryId);
 }
--- demo/src/main/java/com/drtx/demo/core/ports/in/catalog/PublicationServicePort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/in/catalog/PublicationServicePort.java
@@ -1,5 +1,16 @@
 package com.drtx.demo.core.ports.in.catalog;
 
-public interface PublicationServicePort {
+import com.drtx.demo.core.model.catalog.Publication;
 
+import java.util.List;
+
+public interface PublicationServicePort {
+    Publication createPublication(Publication publication);
+    void editPublication(Publication publication);
+    void deletePublication(Long id);
+    Publication findPublicationById(Long id);
+    List<Publication> findAllPublications(); // listado general
+    List<Publication> findPublicationsByCategory(Long categoryId);
+    List<Publication> findPublicationsByUser(Long userId);
+    List<Publication> searchPublications(String keyword);
 }
