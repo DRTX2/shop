2adf10a8e8b3ef2cf9c74be2efe15c3ca1adefa2
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-11 17:12:06 -0500

    Add rating ports

--- demo/src/main/java/com/drtx/demo/core/ports/in/rating/RatingServicePort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/in/rating/RatingServicePort.java
@@ -1,4 +1,13 @@
 package com.drtx.demo.core.ports.in.rating;
 
+import com.drtx.demo.core.model.rating.Rating;
+
+import java.util.List;
+
 public interface RatingServicePort {
+    Rating createRating(Rating rating);
+    List<Rating> findRatingsByUser(Long userId);
+    List<Rating> findRatingsByPublication(Long publicationId);
+    double calculateAverageRatingForUser(Long userId);// ¿Qué tan bien calificado es el usuario?
+    double calculateAverageRatingForPublication(Long publicationId);// ¿Qué tan bien calificada fue una publicación específica?
 }
