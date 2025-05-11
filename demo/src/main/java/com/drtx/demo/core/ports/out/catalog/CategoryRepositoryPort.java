1014b46a9e9140ded6d285b50140b03c4a2bd1fc
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-11 17:59:10 -0500

    Add catalog repository port

--- demo/src/main/java/com/drtx/demo/core/ports/out/catalog/CategoryRepositoryPort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/out/catalog/CategoryRepositoryPort.java
@@ -1,4 +1,14 @@
 package com.drtx.demo.core.ports.out.catalog;
 
+import com.drtx.demo.core.model.catalog.Category;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepositoryPort {
+    Category save(Category category);
+    Category update(Category category);
+    Optional<Category> findById(Long id);
+    List<Category> findAll();
+    void deleteById(Long categoryId);
 }
--- demo/src/main/java/com/drtx/demo/core/ports/out/catalog/PublicationRepositoryPort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/out/catalog/PublicationRepositoryPort.java
@@ -1,4 +1,17 @@
 package com.drtx.demo.core.ports.out.catalog;
 
+import com.drtx.demo.core.model.catalog.Publication;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PublicationRepositoryPort {
+    Publication save(Publication publication);
+    Publication update(Publication publication);
+    void deleteById(Long id);
+    Optional<Publication> findById(Long id);
+    List<Publication> findAll();
+    List<Publication> findByCategoryId(Long categoryId);
+    List<Publication> findByUserId(Long userId);
+    List<Publication> searchByKeyword(String keyword);
 }
