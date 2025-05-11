16db87db001a88aa554b16bcf6489e629fbb8ae0
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-11 17:58:15 -0500

    Add transaction repository port

--- demo/src/main/java/com/drtx/demo/core/ports/out/transaction/PaymentMethodRepositoryPort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/out/transaction/PaymentMethodRepositoryPort.java
@@ -1,4 +1,14 @@
 package com.drtx.demo.core.ports.out.transaction;
 
+import com.drtx.demo.core.model.transaction.PaymentMethod;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PaymentMethodRepositoryPort {
+    PaymentMethod save(PaymentMethod method);
+    PaymentMethod update(PaymentMethod method);
+    Optional<PaymentMethod> findById(Long id);
+    List<PaymentMethod> findAllByUser(Long userId);
+    void deleteById(Long id);
 }
--- demo/src/main/java/com/drtx/demo/core/ports/out/transaction/TransactionRepositoryPort.java
+++ demo/src/main/java/com/drtx/demo/core/ports/out/transaction/TransactionRepositoryPort.java
@@ -1,4 +1,16 @@
 package com.drtx.demo.core.ports.out.transaction;
 
+import com.drtx.demo.core.model.transaction.Transaction;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface TransactionRepositoryPort {
+    Transaction save(Transaction transaction);
+    Optional<Transaction> findById(Long id);
+    List<Transaction> findAllByUserId(Long userId);
+    List<Transaction> findByUserIdBetweenDates(Long userId, LocalDateTime from, LocalDateTime to);
+    void deleteById(Long id);
+    void updateStateToCanceled(Long transactionId);
 }
